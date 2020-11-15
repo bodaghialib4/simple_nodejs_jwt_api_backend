7438fdf60ab1f4e5f3fa6a08865768ff6f06e655
bodaghialib4 <bodaghialib4@gmail.com>
2020-11-15 18:19:22 +0330

    create static data in data.js (regular, special, users)

---
+++ data.js
@@ -0,0 +1,95 @@
+const regular = [
+  {
+    id: 1,
+    content: 'Use a Coding Standard.'
+  },
+  {
+    id: 2,
+    content: 'Write Useful Comments.'
+  },
+  {
+    id: 3,
+    content: 'Refactor codes.'
+  },
+  {
+    id: 4,
+    content: 'Avoid Global Code.'
+  },
+  {
+    id: 5,
+    content: 'Use Meaningful Names.'
+  },
+  {
+    id: 6,
+    content: 'Use Meaningful Structures.'
+  },
+  {
+    id: 7,
+    content: 'Use Version Control Software.'
+  },
+  {
+    id: 8,
+    content: 'Use Automated Build Tools.'
+  },
+  {
+    id: 9,
+    content: 'Use Code Documenters.'
+  },
+  {
+    id: 10,
+    content: 'Use a Testing Framework.'
+  },
+];
+const special = [
+  {
+    id: 11,
+    content: 'Start With the Basics and Spend More Time There.'
+  },
+  {
+    id: 12,
+    content: 'Coding by Hand Is Highly Recommended.'
+  },
+  {
+    id: 13,
+    content: 'Fiddle Around With the Codes.'
+  },
+  {
+    id: 14,
+    content: 'Reach Out to Others for Guidance.'
+  },
+  {
+    id: 15,
+    content: 'Take Breaks to Get Refreshed.'
+  },
+  {
+    id: 16,
+    content: 'Use Online Resources.'
+  },
+  {
+    id: 17,
+    content: 'Read Relevant Material.'
+  },
+  {
+    id: 18,
+    content: 'Learn From the Bugs.'
+  },
+  {
+    id: 19,
+    content: 'Never Get Tired of Practicing.'
+  },
+  {
+    id: 20,
+    content: 'Keep Your Brain Cool.'
+  }
+];
+
+const users = [
+  {
+    'name': 'ali', 'password': 'ali'
+  },
+  {
+    'name': 'example', 'password': 'qwerty'
+  }
+]
+
+module.exports = {regular: regular, special: special, users: users}
